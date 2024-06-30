c4695cc9b3a49f639e0c428fc1bbbbb8d76eb1e8
Siyam hosan <Siyam.hosan@yahoo.com>
2024-06-30 14:16:37 +0600

    feat: Update order status route and response types

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,78 +1,84 @@
 export interface OrderState {
-  id: number
+  id: number;
 
-  browserLaunched: boolean
-  pageOpened: boolean
-  portalUrl: string
-  loggedIn: boolean
-  loggedInUser: string
+  browserLaunched: boolean;
+  pageOpened: boolean;
+  portalUrl: string;
+  loggedIn: boolean;
+  loggedInUser: string;
 
-  proceedToPayUrl: string
+  proceedToPayUrl: string;
 
-  orderSuccess: boolean
+  orderSuccess: boolean;
 
-  orderFailed: boolean
-  orderFailedMessage: string
-  orderFailedErrorCode: number | void
-  orderFailedProof: string | void
+  orderFailed: boolean;
+  orderFailedMessage: string;
+  orderFailedErrorCode: number | void;
+  orderFailedProof: string | void;
+
+  took: number;
 }
 
 export interface SequenceState {
-  index: number
-  sequenceId: number
+  index: number;
+  sequenceId: number;
 
-  pageUrl: string
-  pageOpened: boolean
+  pageUrl: string;
+  pageOpened: boolean;
 
-  stockAmount: number
-  stockSerial: string
-  stockUseSuccess: boolean
-  stockAlreadyUsed: boolean
-  stockAlreadyUseProof: string | void
-  stockTyped: boolean
-  stockEntered: boolean
+  stockAmount: number;
+  stockSerial: string;
+  stockSerialType: string;
+  stockUseSuccess: boolean;
+  stockAlreadyUsed: boolean;
+  stockAlreadyUseProof: string | void;
+  stockTyped: boolean;
+  stockEntered: boolean;
 
-  applyBackUp: boolean
-  backupStockSerial: string
-  backupStockUseSuccess: boolean
-  backupStockAlreadyUsed: boolean
-  backupStockAlreadyUseProof: string
-  backupStockTyped: boolean
-  backupStockEntered: boolean
+  applyBackUp: boolean;
+  backupStockSerial: string;
+  backupStockSerialType: string;
+  backupStockUseSuccess: boolean;
+  backupStockAlreadyUsed: boolean;
+  backupStockAlreadyUseProof: string;
+  backupStockTyped: boolean;
+  backupStockEntered: boolean;
 
-  paymentSuccess: boolean
-  paymentProof: string
+  paymentSuccess: boolean;
+  paymentProof: string;
+
+  retryCount: number;
 }
 
 export type OrderEvent = {
-  event: string
-  status: 'update' | 'finish' | 'failed' | 'placed'
-  errorCode?: number
-  message: string
-  state: OrderState
-  sequences: Record<number, SequenceState>
-}
+  event: string;
+  status: "update" | "finish" | "failed" | "placed" | "success" | "error";
+  errorCode?: number;
+  message: string;
+  state: OrderState;
+  sequences: Record<number, SequenceState>;
+};
 
 export type SequenceEvent = {
-  sequence: number
-  order: number
+  sequence: number;
+  order: number;
 } & (
   | {
-      event: 'sequence.redeemed'
-      redeemState: 'success' | 'used' | 'unsupported'
-      isBackup: boolean
+      event: "sequence.redeemed";
+      redeemState: "success" | "used" | "unsupported";
+      isBackup: boolean;
     }
   | {
-      event: 'sequence.update'
-      state: SequenceState
+      event: "sequence.update";
+      state: SequenceState;
     }
-)
+);
 
 export type OrderCallbackPayload = {
-  type: 'placed' | 'update' | 'finished'
+  type: "placed" | "update" | "finished" | "success" | "error";
   data: {
-    status: 'update' | 'finish' | 'failed' | 'placed'
-    orderState: OrderState
-    sequenceStates: Record<number, SequenceState>
-  }
-}
+    status: "update" | "finish" | "failed" | "placed" | "success" | "error";
+    orderState: OrderState;
+    sequenceStates: Record<number, SequenceState>;
+  };
+};
