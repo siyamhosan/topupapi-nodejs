f5c2d01ac7f0a7aae8e5a8eafb139cb3ae8d62da
Siyam hosan <Siyam.hosan@yahoo.com>
2024-12-09 05:45:33 +0600

    feat: Add additional supported games for Free Fire

--- src/types/init.ts
+++ src/types/init.ts
@@ -6,7 +6,12 @@ export const UserRole = z.enum(UserRoles);
 export const StockTypes = ["UNIPIN_VOUCHER", "UNIPIN_GIFT_CARD"] as const;
 export const StockType = z.enum(StockTypes);
 
-export const SupportedGames = ["FREE_FIRE"] as const;
+export const SupportedGames = [
+  "FREE_FIRE",
+  "FREE_FIRE_SHELL",
+  "FREE_FIRE_SG",
+  "FREE_FIRE_ID",
+] as const;
 export const SupportedGame = z.enum(SupportedGames);
 
 export const SupportedRegions = ["MY", "SG", "ID"] as const;
